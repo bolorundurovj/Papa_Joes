366b8e460edea984990e49fde9fe82ab66cea432
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2019-12-27 19:14:38 +0100

    New commit

---
+++ myvirtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/users/bolorundurovb/desktop/edxprojects/project3/myvirtualenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
